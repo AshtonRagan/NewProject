014b7f1faac3fa9ee4fc2378425ef53cf0e24e45
AshtonRagan <arkjr1@gmail.com>
2020-08-19 15:43:00 -0500

    the setup

---
+++ src/navBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+function navBar() {
+  return (
+    <div>
+      <nav>
+        <span>Home</span>
+        <span>Store</span>
+        <span>Students</span>
+      </nav>
+    </div>
+  );
+}
+export default navBar;
